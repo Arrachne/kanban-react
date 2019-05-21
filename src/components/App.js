7a59764d7ff51bcc25f7c70a97c7e2529488b00b
Yana Kozlovskaya <yana-koz@bk.ru>
2019-05-22 00:58:46 +0500

    data array added. columns and tasks are created on init

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Column from "./Column"
 
-function App() {
-  return (
-    <div className='kanban'>
-      <Column/>
-    </div>
-  );
+class App extends Component {
+    state = {
+        data: [
+            {
+                name: "To do",
+                tasks: ["Save the world"]
+            },
+            {
+                name: "In progress",
+                tasks: ["Learn js", "Learn React"]
+            },
+            {
+                name: "",
+                tasks: []
+            }
+        ]
+    }
+
+    render() {
+        var columns = this.state.data.map((column, index) => 
+            <Column key = {index} name={column.name} taskList={column.tasks} />
+        )
+
+        return (
+            <div className='kanban' >
+                {columns}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import Task from "./Task" 
-import ButtonCreateOffer from "./ButtonCreateOffer" 
+import React, { Component } from 'react';
+import Task from "./Task"
+import ButtonCreateOffer from "./ButtonCreateOffer"
 import FromCreateNewElement from "./FromCreateNewElement"
 
-function Column() {
-    return (
-        <div className='column'>
-            <div className="col-name">
-                TO DO
-            </div>
-            <div className="column-inner">
-                <Task text='1'/>
-                <Task text='2'/>
-                <Task text='3'/>
-            </div>
-            <ButtonCreateOffer />
-            <FromCreateNewElement />
+class Column extends Component {    
+    state = {
+        isFormHidden: true,
+        newElementType: ''
+    }
+
+    render() {
+        var { name, taskList } = this.props
+
+        var tasks = taskList.map((task, index) =>
+            <Task key={index} text={task} />
+        )
+
+        var head = (<div className="col-name">
+            {name}
         </div>
-    );
+        )
+        var inner = (<div className="column-inner">
+                        {tasks}
+                    </div>
+        )
+
+        // тип эл-та, который должна создать форма
+        // taskList[0] показывает, явл-ся ли колонка активной. но на самом деле надо отдельный атрибут в app
+        var newElemType = taskList[0] ? 'task' : 'column'
+
+        return (
+            <div className='column' >
+                {name ? head : ''}
+                {taskList[0] ? inner : ''}
+                <ButtonCreateOffer isBtnHidden={!this.state.isFormHidden} onClick={this.handleClick.bind(this, 'createOfferBtn')} newElemType = {newElemType} />
+                <FromCreateNewElement isFormHidden={this.state.isFormHidden} onClick={this.handleClick.bind(this, 'rejectBtn')} newElemType = {newElemType}/>
+            </div>
+        )
+    }
+
+
+    handleClick = (btn) => {
+        console.log(btn, ' pressed')
+        this.setState({
+            isFormHidden: !this.state.isFormHidden,
+        })
+    }
+
 }
 
 export default Column;
